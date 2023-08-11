2c3796e4a176b59743969fd8d5bc9528821d5d2e
Dima Holovachenko <dmytro.holovachenko@infotech.gov.ua>
2023-08-11 08:58:34 +0300

    improve validation

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -2,11 +2,20 @@ import { ValidationError, validationResult } from 'express-validator'
 import { Request } from 'express'
 import ApiError from '../errors/ApiError'
 
-const errorFormatter = ({ msg, param }: ValidationError) => `${param}: ${msg}`
+const errorFormatter = (error: ValidationError) => {
+  switch (error.type) {
+    case 'field':
+      // this is a FieldValidationError
+      return `${error.path}: ${error.msg}`
+    default:
+      // Not a known type.
+      return `Unexpected error ${error.type}: ${error.msg}`
+  }
+}
 
 export const checkValidationError = (req: Request) => {
   const errors = validationResult(req).formatWith(errorFormatter)
   if (!errors.isEmpty()) {
-    throw ApiError.badRequestError(errors.array().join('\n'))
+    throw ApiError.badRequestError(`Помилки валідації -  ${errors.array().join(';')}`)
   }
 }
